3ed33841d982c52c9df5f8cd2eb1f3ecb478f7a6
Shubham Desai <shubham.desai@torinit.ca>
2023-07-26 20:38:58 +0530

    Initial Eslint, Prettier, Husky setup added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-export default function Home() {
+import React from "react";
+export default function Home(): React.JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
