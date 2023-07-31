a6969f32dd6adba6687b11325f4a6632c82a3b82
Shubham Desai <shubham.desai@torinit.ca>
2023-07-31 11:40:18 +0530

    feat: lazyimage and songCard component added.

--- testing/Nav.test.tsx
+++ testing/Nav.test.tsx
@@ -1,7 +1,7 @@
+import Nav from "@/components/Nav";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import Nav from ".";
 
 describe("display  nav component", () => {
   it("renders a nav", () => {
