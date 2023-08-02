91c9ef5f8dd5b1fb5ba86dc1db2efe914d273e73
Shubham Desai <shubham.desai@torinit.ca>
2023-08-02 13:26:04 +0530

    test cases added for nav and lazyimage component

---
+++ src/testing/Songcard.test.tsx
@@ -0,0 +1,37 @@
+import SongCard from "@/components/SongCard";
+import { mockSongData } from "@/constants/index";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+describe("The card component tests", () => {
+  it("The card should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("card")).toBeInTheDocument();
+  });
+
+  it("The card image should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("image")).toBeInTheDocument();
+  });
+
+  it("The song name  should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("song-name")).toBeInTheDocument();
+  });
+
+  it("The artist name should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("artist-name")).toBeInTheDocument();
+  });
+
+  it("The play button should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("play-button")).toBeInTheDocument();
+  });
+
+  it("The like button should be in the document", () => {
+    render(<SongCard song={mockSongData} />);
+    expect(screen.getByTestId("like-button")).toBeInTheDocument();
+  });
+});
