d073540abf61f8136f7938d59a93a96aba7cf0d0
Shubham Desai <shubham.desai@torinit.ca>
2023-08-08 17:28:42 +0530

    fix: build issue fixed

--- src/app/songs/[songId]/page.tsx
+++ src/app/songs/[songId]/page.tsx
@@ -8,20 +8,6 @@ import store from "@/src/ducks/store";
 import { setCurrentSong } from "@/src/reducers/SongReducer";
 const prisma = new PrismaClient();
 
-export const revalidate = 60;
-
-export async function generateStaticParams(): Promise<
-  Array<{
-    songId: string;
-  }>
-> {
-  const songIds = await prisma.song.findMany();
-  return songIds.map((song) => {
-    const id = song.songId;
-    return { songId: String(id) };
-  });
-}
-
 const SongPage = async ({
   params,
 }: {
@@ -30,7 +16,7 @@ const SongPage = async ({
   try {
     const songRes = await prisma.song.findFirst({
       where: {
-        songId: { equals: Number(params.songId) },
+        id: { equals: params.songId },
       },
     });
 
